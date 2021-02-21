3557b5f6a6a0e4648c6c13fd14cfd9254d77650c
abdoa <abdoa@DESKTOP-K1UH5CG>
2021-02-21 14:59:51 +0200

    21/2/2021

--- Almotkaml.HR/Almotkaml.HR.Models/SettingsModel.cs
+++ Almotkaml.HR/Almotkaml.HR.Models/SettingsModel.cs
@@ -7,109 +7,129 @@ namespace Almotkaml.HR.Models
     public class SettingsModel
     {
 
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-     ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         public string TextboxFrom { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-     ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
-        public string TextboxTo { get; set; }
-
-        public string NumberCheck { get; set; }
         
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-     ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
-        public int Number { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-          ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
-        [Display(ResourceType = typeof(Title), Name = nameof(Title.DateSpent))]
-        public string Date { get; set; }
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+        public string TextboxTo { get; set; }
+        
+        public string NumberCheck { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+        public int Number { get; set; }
+        
+        [Required(ErrorMessageResourceType = typeof(SharedMessages), ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+        [Display(ResourceType = typeof(Title), Name = nameof(Title.DateSpent))] 
+        public string Date { get; set; } 
+        
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.SickVacation))]
         public decimal SickVacation { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-       ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.sickLeave))]
         public decimal SickLeave { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.ExtraWork))]
         public decimal ExtraWork { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.ExtraWorkVacation))]
         public decimal ExtraWorkVacation { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.SolidarityFund))]
         public decimal SolidarityFund { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.EmployeeShareAll))]
         public decimal EmployeeShareAll { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.EmployeeShareReduced))]
         public decimal EmployeeShareReduced { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.EmployeeShareWithoutReduced))]
         public decimal EmployeeShareWithoutReduced { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.EmployeeShareReduced35Year))]
         public decimal EmployeeShareReduced35Year { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.CompanyShareAll))]
         public decimal CompanyShareAll { get; set; }
 
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.SafeAll))]
         public decimal SafeShareAll { get; set; }
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.SafeReducer))]
         public decimal SafeShareReduced { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.CompanyShareReduced))]
         public decimal CompanyShareReduced { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.CompanyShareWithoutReduced))]
         public decimal CompanyShareWithoutReduced { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.CompanyShareReduced35Year))]
         public decimal CompanyShareReduced35Year { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.JihadTax))]
         public decimal JihadTax { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.ExemptionTaxOne))]
         public decimal ExemptionTaxOne { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.ExemptionTaxTwo))]
         public decimal ExemptionTaxTwo { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.IncomeTaxOne))]
         public decimal IncomeTaxOne { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.IncomeTaxTwo))]
         public decimal IncomeTaxTwo { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.StampTax))]
         public decimal StampTax { get; set; }
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+
+        [RegularExpression(@"^[0-9.]*$", ErrorMessage = "االرجاء إدخال ارقام فقط")]
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.ChilderPermium))]
         public decimal ChilderPermium { get; set; }
+         
 
 
 
@@ -122,13 +142,13 @@ namespace Almotkaml.HR.Models
         public bool Friday { get; set; }
         public bool CanSubmit { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(SharedMessages),
-         ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
+        
+        [Required(ErrorMessageResourceType = typeof(SharedMessages),ErrorMessageResourceName = nameof(SharedMessages.IsRequired))]
         [Display(ResourceType = typeof(Title), Name = nameof(Title.VacationIncludesHolidays))]
-        public bool VacationIncludesHolidays { get; set; }
+        public bool VacationIncludesHolidays { get; set; } 
+       
         public decimal Grouplife { get; set; }
 
- 
         [Display(ResourceType = typeof(Title), Name = nameof(Title.BackUpPath))]
         public string BackUpPath { get; set; }
     }
